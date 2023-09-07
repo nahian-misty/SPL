62da5ae76bc5ec5280f7372e5720285c2e465f5b
nahian-misty <140546458+nahian-misty@users.noreply.github.com>
2023-09-07 14:00:30 +0600

    Add files via upload

---
+++ createMAZEvisualizer.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <graphics.h>
+#define ROWS 40
+#define COLS 40
+
+int maze[ROWS][COLS] = {
+    {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,1,1,1,0,1,1,0,0,0,0,0,0,0},
+    {1,1,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
+    {1,1,1,1,1,0,1,1,1,0,0,0,0,0,1,1,1,1},
+    {1,1,1,0,0,0,1,1,1,0,1,1,1,0,1,1,0,1},
+    {1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,0,0,1},
+    {1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1},
+    {1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1},
+    {1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,1},
+    {1,0,1,1,1,0,1,0,1,1,0,1,1,1,1,1,0,1},
+    {1,0,1,1,1,0,1,0,0,1,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+
+};
+
+
+void visualizeMaze() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j <COLS; j++) {
+            int x = j * 35;
+            int y = i * 35;
+
+            if (maze[i][j] == 1) {
+                rectangle(x, y, x + 35, y + 35);
+            } else if (maze[i][j] == 2) {
+                setcolor(YELLOW);
+                 setfillstyle(SOLID_FILL, RED);
+                //circle(x + 17, y + 17, 15);
+                rectangle(x+5, y+5, x + 30, y + 30);
+                //floodfill(312,415,15);
+                setcolor(WHITE);
+            }
+        }
+    }
+}
+
+
+int main() {
+
+    int graphicsDriver = DETECT, graphicsMode;
+    initgraph(&graphicsDriver, &graphicsMode, "");
+
+    cleardevice();
+
+    visualizeMaze();
+
+    getch();
+    closegraph();
+
+    return 0;
+}
+
