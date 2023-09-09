6363a4d67ff8c3f196232d45a90aada88d3ae32f
nahian-misty <140546458+nahian-misty@users.noreply.github.com>
2023-09-09 12:52:48 +0600

    Add files via upload

---
+++ mazeWithKey.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <graphics.h>
+#define ROWS 40
+#define COLS 40
+
+int maze[ROWS][COLS] = {
+    {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1},
+    {0,0,0,0,0,1,1,1,0,1,1,0,0,0,0,0,0,0},
+    {1,1,1,1,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
+    {1,1,1,1,1,0,1,1,1,0,0,0,0,0,1,1,1,1},
+    {1,1,1,0,0,0,1,1,1,0,1,1,1,0,1,1,0,1},
+    {1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,0,0,1},
+    {1,1,1,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1},
+    {1,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1},
+    {1,0,0,0,1,0,1,0,0,0,0,1,1,0,0,0,0,1},
+    {1,0,1,1,1,0,1,0,1,1,0,1,1,1,1,1,0,1},
+    {1,0,1,1,1,0,1,0,0,1,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1},
+
+};
+
+int win= 0;
+int path[12][18];
+
+void visualizeMaze() {
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j <COLS; j++) {
+            int x = j * 35;
+            int y = i * 35;
+
+            if (maze[i][j] == 1) {
+                rectangle(x, y, x + 35, y + 35);
+            } else if (maze[i][j] == 2) {
+              for(int k=0;k<3;k++){
+                         setcolor(YELLOW);
+
+                        // rectangle(x+k+5, y+k+5, x + 30-k, y + 30-k);
+
+                        line(290+k,385+k,290+k,420+k);
+                        //setcolor(RED);
+                        setfillstyle(SOLID_FILL, RED);
+                        line(290+k,385+k,310+k,400+k);
+                        line(310+k,400+k,290+k,400+k);
+                       // floodfill(300,295,10);
+
+                }
+
+                setcolor(WHITE);
+            }
+        }
+    }
+}
+
+void printPaths(int row, int col) {
+    if (row < 0 || col < 0 || row >= 12 || col >= 18 || win || maze[row][col] == 1 || path[row][col] == 1) {
+        return;
+    }
+
+    path[row][col] = 1;
+
+    if (maze[row][col] == 2) {
+        win = 1;
+        return;
+    }
+
+    printPaths(row + 1, col);
+    printPaths(row, col + 1);
+    printPaths(row, col - 1);
+    printPaths(row - 1, col);
+
+    if (!win) {
+        path[row][col] = 0;
+    }
+}
+
+void drawPathOnMaze() {
+    for(int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            if (path[i][j] == 1) {
+                setfillstyle(SOLID_FILL, MAGENTA);
+                delay(100);
+                bar(j * 35 + 5, i * 35 + 5, (j + 1) * 35 - 5, (i + 1) * 35 - 5);
+            }
+
+        }
+    }
+}
+
+void manualPLAY(){
+    int i=0,j=8;
+    int x=298,y=18,rad=12;
+    while(1){
+        circle(x,y,rad);
+
+    //cleardevice();
+
+    if(GetAsyncKeyState(VK_RIGHT) && maze[i][j+1]==0){
+        x=x+35;
+        j=j+1;
+    }
+    else if(GetAsyncKeyState(VK_LEFT) && maze[i][j-1]==0){
+        x=x-35;
+        j=j-1;
+    }
+    else if(GetAsyncKeyState(VK_UP) && maze[i-1][j]==0){
+        y=y-35;
+        i=i-1;
+    }
+    else if(GetAsyncKeyState(VK_DOWN) && maze[i+1][j]==0){
+        y=y+35;
+        i=i+1;
+    }
+     else if(GetAsyncKeyState(VK_LEFT) && maze[i][j-1]==1){
+        outtextxy(200, 450, "you failed...the actual path to win was-");
+        drawPathOnMaze();
+    }
+    else if(GetAsyncKeyState(VK_RIGHT) && maze[i][j+1]==1){
+         outtextxy(200, 450, "you failed...the actual path to win was-");
+        drawPathOnMaze();
+    }
+     else if(GetAsyncKeyState(VK_UP) && maze[i-1][j]==1){
+        outtextxy(200, 450, "you failed...the actual path to win was-");
+        drawPathOnMaze();
+    }
+     else if(GetAsyncKeyState(VK_DOWN) && maze[i+1][j]==1){
+        outtextxy(200,450, "you failed...the actual path to win was-");
+        drawPathOnMaze();
+    }
+     else if(GetAsyncKeyState(VK_DOWN) && maze[i+1][j]==2){
+        outtextxy(200,400, "CONGRATULATIONS!!!!YOU HAVE SOLVED THE MAZE!!!");
+
+    }
+
+    else if(GetAsyncKeyState(VK_ESCAPE)){
+        break;
+    }
+
+delay(100);
+
+
+    }
+}
+
+int main() {
+    int startRow;
+    int startCol;
+     int k=0;
+//searching for the start point
+        for (int j = 0; j < 18; j++){
+            if(maze[k][j]==0){
+                     startRow =k;
+                     startCol = j;}
+        }
+        //printf("%d\n%d\n",startingRow,startingCol);
+
+    int graphicsDriver = DETECT, graphicsMode;
+    initgraph(&graphicsDriver, &graphicsMode, "");
+
+    cleardevice();
+
+    visualizeMaze();
+
+    printPaths(startRow, startCol);
+
+    if (win) {
+        printf("Path to reach the goal:\n") ;
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 18; j++) {
+                if (path[i][j] == 1) {printf("->(%d,%d) ",i,j) ;}
+            }
+            printf("\n") ;
+        }
+    } else {
+         printf("Path to the goal not found...\n");
+    }
+
+    //if (win) {
+       // drawPathOnMaze();
+   // }
+  // visualizeMaze();
+
+    manualPLAY();
+    getch();
+    closegraph();
+
+    return 0;
+}
+
+
